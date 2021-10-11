8ee76c9995c25a3570e8a30e04901c89c5538cc9
AleXXX_007 <AleXXX_007@inbox.ru>
2021-10-11 21:22:46 +0400

    remove default view

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('dashboard');
+})->middleware(['auth']);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
