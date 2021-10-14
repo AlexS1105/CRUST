e0374e4352145ca2d5e082b234ad4fc169bd0b00
AleXXX_007 <AleXXX_007@inbox.ru>
2021-10-14 18:58:23 +0400

    characters -> my characters

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -13,7 +13,7 @@
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-nav-link :href="route('characters.index')" :active="request()->routeIs('characters.*')">
-                        {{ __('Characters') }}
+                        {{ __('My Characters') }}
                     </x-nav-link>
                 </div>
             </div>
@@ -64,7 +64,7 @@
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('characters.index')" :active="request()->routeIs('characters.*')">
-                {{ __('Characters') }}
+                {{ __('My Characters') }}
             </x-responsive-nav-link>
         </div>
 
