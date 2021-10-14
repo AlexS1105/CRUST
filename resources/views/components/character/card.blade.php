2e3dd71d38e21d41745310d50052cfa06cac7dc4
AleXXX_007 <AleXXX_007@inbox.ru>
2021-10-14 19:08:41 +0400

    hover transform

--- resources/views/components/character/card.blade.php
+++ resources/views/components/character/card.blade.php
@@ -1,4 +1,4 @@
-<a href="{{ route('characters.show', $character->login)}}" class="bg-white rounded-3xl shadow-lg max-w-sm overflow-hidden pb-4">
+<a href="{{ route('characters.show', $character->login)}}" class="bg-white rounded-3xl shadow-lg max-w-sm overflow-hidden pb-4 transition duration-150 ease-in-out transform hover:-translate-y-2 hover:scale-105">
   <div class = "relative">
     <div class="absolute top-0 right-0 m-4 text-lg">
       <x-character.status :status="$character->status"/>
