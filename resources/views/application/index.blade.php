87df96374bf8b9daa79018ea336c00957a36627b
AleXXX_007 <AleXXX_007@inbox.ru>
2021-10-15 15:27:53 +0400

    view work

--- resources/views/application/index.blade.php
+++ resources/views/application/index.blade.php
@@ -6,8 +6,8 @@
   </x-slot>
 
   <div class="py-8 max-w-4xl mx-auto">
-    <div class="bg-white rounded-xl shadow-lg p-4 w-auto">
-      <div class="flex mb-4 gap-4">
+    <div class="bg-white rounded-xl shadow-lg p-6 w-auto">
+      <div class="flex mb-4 gap-4 justify-center">
         @foreach (App\Enums\CharacterStatus::getInstances() as $_status)
           <a href="{{ route('applications.index', [ 'status' => $_status->value ]) }}" class="{{ $status == $_status ?:"opacity-40" }}">
             <x-character.status :status=$_status/>
@@ -15,44 +15,56 @@
         @endforeach
       </div>
 
-      <table class="table-auto w-full border">
-        <thead class="border bg-gray-200">
-          <tr>
-            <th class="px-4 py-2 border border-gray-400">
-              Character
-            </th>
-            <th class="px-4 py-2 border border-gray-400">
-              Player
-            </th>
-            <th class="px-4 py-2 border border-gray-400">
-              Time
-            </th>
-            <th class="px-4 py-2 border border-gray-400">
-              Actions
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          @foreach ($characters as $character)
-            <tr class="py-2 border hover:bg-gray-100 text-center">
-              <td class="px-4 py-2 border">
-                <a class="font-bold underline text-blue-600 visited:text-purple-600" href="{{ route('characters.show', $character->login) }}">
-                  {{ $character->name }}
-                </a>
-              </td>
-              <td class="px-4 py-2 border">
-                {{ $character->user->name }}
-              </td>
-              <td class="px-4 py-2 border">
-                {{ Carbon\Carbon::parse($character->status_updated_at)->diffForHumans() }}
-              </td>
-              <td class="px-4 py-2 border">
-                {{-- TODO: Application Actions --}}
-              </td>
+      @if(count($characters))
+        <table class="table-auto w-full border">
+          <thead class="border bg-gray-200">
+            <tr>
+              <th class="px-4 py-2 border border-gray-400">
+                Character
+              </th>
+              <th class="px-4 py-2 border border-gray-400">
+                Player
+              </th>
+              <th class="px-4 py-2 border border-gray-400">
+                Registrar
+              </th>
+              <th class="px-4 py-2 border border-gray-400">
+                Time
+              </th>
+              <th class="px-4 py-2 border border-gray-400">
+                Actions
+              </th>
             </tr>
-          @endforeach
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            @foreach ($characters as $character)
+              <tr class="py-2 border hover:bg-gray-100 text-center">
+                <td class="px-4 py-2 border">
+                  <a class="font-bold underline text-blue-600 visited:text-purple-600" href="{{ route('characters.show', $character->login) }}">
+                    {{ $character->name }}
+                  </a>
+                </td>
+                <td class="px-4 py-2 border">
+                  {{ $character->user->name }}
+                </td>
+                <td class="px-4 py-2 border">
+                  {{ $character->registrar ? $character->registrar->name : "" }}
+                </td>
+                <td class="px-4 py-2 border">
+                  {{ Carbon\Carbon::parse($character->status_updated_at)->diffForHumans() }}
+                </td>
+                <td class="px-4 py-2 border">
+                  {{-- TODO: Application Actions --}}
+                </td>
+              </tr>
+            @endforeach
+          </tbody>
+        </table>
+      @else
+        <p class="pt-4 text-xl font-semibold text-gray-500 text-center">
+          No characters found
+        </p>
+      @endif
 
       <div class="pt-4">
         {{ $characters->appends(request()->query())->links() }}
