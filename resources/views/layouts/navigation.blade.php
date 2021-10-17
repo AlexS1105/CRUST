0e0c6b8e772865dad394c4d341d6a58998603c91
AleXXX_007 <AleXXX_007@inbox.ru>
2021-10-17 18:08:22 +0400

    event extracted

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -49,9 +49,7 @@
             <form method="POST" action="{{ route('logout') }}">
               @csrf
 
-              <x-dropdown-link :href="route('logout')"
-                  onclick="event.preventDefault();
-                        this.closest('form').submit();">
+              <x-dropdown-link :href="route('logout')">
                 {{ __('Log Out') }}
               </x-dropdown-link>
             </form>
