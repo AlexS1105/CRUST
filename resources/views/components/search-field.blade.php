2e49bb80acb9833b5f1924d92c26103b9d6f9b23
AleXXX_007 <AleXXX_007@inbox.ru>
2021-10-17 13:09:22 +0400

    margin changed

--- resources/views/components/search-field.blade.php
+++ resources/views/components/search-field.blade.php
@@ -9,7 +9,7 @@
         value="{{ isset($search) ? $search : "" }}"
       />
 
-      <svg xmlns="http://www.w3.org/2000/svg" class="absolute right-4 h-6 w-6 text-gray-400 pointer-events-none" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+      <svg xmlns="http://www.w3.org/2000/svg" class="absolute right-3 h-6 w-6 text-gray-400 pointer-events-none" fill="none" viewBox="0 0 24 24" stroke="currentColor">
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
       </svg>
     </div>
