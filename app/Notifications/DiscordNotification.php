cf31e62c18fc81a2befecc1496048db6416ea8f3
AleXXX_007 <AleXXX_007@inbox.ru>
2021-10-24 19:09:51 +0400

    extract discord notification

--- app/Notifications/DiscordNotification.php
+++ app/Notifications/DiscordNotification.php
@@ -3,31 +3,24 @@
 namespace App\Notifications;
 
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
 use NotificationChannels\Discord\DiscordChannel;
-use NotificationChannels\Discord\DiscordMessage;
 
 class DiscordNotification extends Notification
 {
     use Queueable;
 
-    public $text;
-
-    public function __construct($text)
-    {
-        $this->text = $text;
-    }
+    public $embed = [
+        'type' => 'rich',
+        'footer' => [
+            'text' => 'Система Тесея',
+            'icon_url' => 'https://i.imgur.com/NlCDmmT.png'
+        ],
+    ];
 
     public function via($notifiable)
     {
         return [DiscordChannel::class];
-    }
-
-    public function toDiscord($notifiable)
-    {
-        return DiscordMessage::create($this->text);
     }
 }
--- app/Notifications/RegisteredNotification.php
+++ app/Notifications/RegisteredNotification.php
@@ -2,39 +2,23 @@
 
 namespace App\Notifications;
 
-use Illuminate\Bus\Queueable;
-use Illuminate\Notifications\Notification;
-
-use NotificationChannels\Discord\DiscordChannel;
 use NotificationChannels\Discord\DiscordMessage;
 
-class RegisteredNotification extends Notification
+class RegisteredNotification extends DiscordNotification
 {
-    use Queueable;
-
-    public function via($notifiable)
-    {
-        return [DiscordChannel::class];
-    }
-
     public function toDiscord($notifiable)
     {
         $profileLink = route('users.edit', $notifiable);
         $createCharacterLink = route('characters.create');
         $appUrl = config('app.url');
-        return DiscordMessage::create('', [
+
+        $embed = [
             'title' => "Добро пожаловать на ролевой проект Тесей, $notifiable->name!",
-            'type' => 'rich',
             'description' => "Вы успешно прошли регистрацию в нашей системе. Чтобы начать игру, Вам необходимо создать персонажа и дождаться его проверки нашими регистраторами.
             
             Для Вас был создан аккаунт на нашей [Вики]($appUrl). Используйте логин **$notifiable->name** и пароль, **указанный при регистрации** или просто нажмите [сюда]($appUrl) для автоматического входа.",
             'url' => $appUrl,
-            'timestamp' => now(),
             'color' => 0x70a7ff,
-            'footer' => [
-                'text' => 'Система Тесея',
-                'icon_url' => 'https://i.imgur.com/NlCDmmT.png'
-            ],
             'fields' => [
                 [
                     'name' => 'Что делать дальше?',
@@ -49,6 +33,8 @@ class RegisteredNotification extends Notification
                     'value' => "Если Вы не хотите получать оповещения от бота в ЛС, отключите эту настройку в своём [профиле]($profileLink)."
                 ]
             ]
-        ]);
+        ];
+        
+        return DiscordMessage::create('', array_merge($this->embed, $embed));
     }
 }
