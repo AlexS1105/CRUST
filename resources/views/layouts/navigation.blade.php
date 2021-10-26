572f1cb0b9cabf4fb2582b7fea4c957b46438d64
AleXXX_007 <AleXXX_007@inbox.ru>
2021-10-26 10:45:47 +0400

    rebranding

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -6,7 +6,7 @@
         <!-- Logo -->
         <div class="flex-shrink-0 flex items-center">
           <a href="{{ route('characters.index') }}">
-            <x-application-logo class="block h-10 w-auto fill-current text-gray-600" />
+            <x-application-logo class="block h-12 w-auto fill-current text-gray-600" />
           </a>
         </div>
 
