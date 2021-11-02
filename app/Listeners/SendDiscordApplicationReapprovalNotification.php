64d34f130614b3103f4d8d9d6016f0b39fa67e8c
AleXXX_007 <AleXXX_007@inbox.ru>
2021-11-02 12:06:10 +0400

    reapproval notification fix

--- app/Listeners/SendDiscordApplicationReapprovalNotification.php
+++ app/Listeners/SendDiscordApplicationReapprovalNotification.php
@@ -13,12 +13,12 @@ class SendDiscordApplicationReapprovalNotification
             $user = $event->user;
             $character = $event->character;
 
-            $notification = new ApplicationReapprovalNotification($character, $user);
-            
-            $character->user->notify($notification);
+            if (!$user->is($character->user)) {
+                $character->user->notify(new ApplicationReapprovalNotification($character, $user));
+            }
 
             if ($character->registrar->id != $user->id) {
-                $character->registrar->notify($notification);
+                $character->registrar->notify(new ApplicationReapprovalNotification($character, $user, true));
             }
         } catch (Exception $e) {
             
