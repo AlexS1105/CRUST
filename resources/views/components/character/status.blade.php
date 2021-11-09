6a7c72350d4986186070305eefff595139a79c97
AleXXX_007 <AleXXX_007@inbox.ru>
2021-11-09 15:10:24 +0400

    make status bar smaller

--- resources/views/components/character/status.blade.php
+++ resources/views/components/character/status.blade.php
@@ -1,3 +1,3 @@
-<div class="px-3 py-1 text-white bg-{{ $status->color() }} ring-4 ring-{{ $status->color() }} ring-opacity-50 rounded-full font-bold whitespace-nowrap">
+<div class="px-2 text-white bg-{{ $status->color() }} ring-4 ring-{{ $status->color() }} ring-opacity-50 rounded-full font-bold whitespace-nowrap text-sm">
   {{ $status->localized() }}
 </div>
