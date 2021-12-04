17149d836cd5e8e4a8eea1265604e102856786ae
AleXXX_007 <AleXXX_007@inbox.ru>
2021-12-04 12:49:45 +0400

    better http requests

--- app/Services/DiscordService.php
+++ app/Services/DiscordService.php
@@ -18,16 +18,12 @@ class DiscordService
         $token = $accessData['access_token'];
 
         $userData = Cache::remember('userData'.$token, 604800, function() use($token) {
-            $curl = curl_init('https://discordapp.com/api/users/@me');
+            $response = Http::withHeaders([
+                'Authorization' => 'Bearer '.$token
+            ])->get('https://discordapp.com/api/users/@me');
+            $response->throw();
 
-            curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-            curl_setopt($curl, CURLOPT_HTTPHEADER, [
-                'Authorization: Bearer '.$token
-            ]);
-            
-            curl_close($curl);
-    
-            return json_decode(curl_exec($curl), true);
+            return json_decode($response->body(), true);
         });
 
         $userData['accessData'] = $accessData;
@@ -37,23 +33,19 @@ class DiscordService
 
     public function getAccessToken($code, $redirect)
     {
-        $curl = curl_init(config('services.discord.api').'/oauth2/token');
-
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($curl, CURLOPT_POST, true);
-        curl_setopt($curl, CURLOPT_POSTFIELDS, [
+        $response = Http::withHeaders([
+            'Content-Type' => 'application/x-www-form-urlencoded',
+            'Accept' => 'application/json'
+        ])->asForm()->post(config('services.discord.api').'/oauth2/token', [
             'client_id' => config('services.discord.clientid'),
             'client_secret' => config('services.discord.secret'),
             'grant_type' => 'authorization_code',
             'code' => $code,
             'redirect_uri' => $redirect
         ]);
+        $response->throw();
 
-        curl_setopt($curl, CURLOPT_HTTPHEADER, [
-            'Content-Type' => 'application/x-www-form-urlencoded'
-        ]);
-
-        $data = json_decode(curl_exec($curl), true);
+        $data = json_decode($response->body(), true);
 
         return $data;
     }
