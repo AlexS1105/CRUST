10563b59592e72968a3320fa02d14babe703bb82
AleXXX_007 <AleXXX_007@inbox.ru>
2021-12-18 14:45:24 +0400

    revoke wiki tokens async

--- app/Http/Controllers/WikiController.php
+++ app/Http/Controllers/WikiController.php
@@ -8,7 +8,7 @@ class WikiController extends Controller
 {
     public function __invoke(Request $request)
     {
-        RevokeOldTokens::dispatchAfterResponse(auth()->user()->id);
+        RevokeOldTokens::dispatch(auth()->user()->id);
         return redirect()->away(auth()->user()->tokens->where('client_id', env('WIKI_CLIENT_ID'))->first() !== null ? env('WIKI_URL') : env('WIKI_OAUTH2_URL'));
     }
 }
