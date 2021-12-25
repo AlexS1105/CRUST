d2640f171d0f662fd18bb6f05f13f285b5920497
AleXXX_007 <AleXXX_007@inbox.ru>
2021-12-25 16:46:06 +0400

    discord tag validation error localize

--- app/Rules/DiscordTag.php
+++ app/Rules/DiscordTag.php
@@ -14,6 +14,6 @@ class DiscordTag implements Rule
 
     public function message()
     {
-        return 'The :attribute must be valid discord tag.';
+        return __('validation.discordtag');
     }
 }
