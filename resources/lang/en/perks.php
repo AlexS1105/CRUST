5104c4352e8b042fc36fee70c326ea87de02e6fd
AleXXX_007 <AleXXX_007@inbox.ru>
2022-01-04 13:04:45 +0400

    localization

---
+++ resources/lang/en/perks.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    'index' => 'Perks',
+    'create' => 'Create Perk',
+    'edit' => 'Edit Perk',
+    'placeholder' => [
+        'name' => 'Perk Name',
+        'description' => 'Perk Effect Description'
+    ],
+    'variants' => [
+        'create' => 'Add Variant',
+        'edit' => 'Edit Perk Variant'
+    ],
+    'types' => [
+        'combat' => 'Combat',
+        'noncombat' => 'Non-Combat',
+        'native' => 'Native',
+        'unique' => 'Unique'
+    ]
+];
