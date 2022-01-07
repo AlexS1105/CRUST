eb143c02c1ee94a95e110f3ff545ad018fe1b6e2
AleXXX_007 <AleXXX_007@inbox.ru>
2022-01-07 13:11:27 +0400

    unused imports removed

--- app/Http/Requests/TraitRequest.php
+++ app/Http/Requests/TraitRequest.php
@@ -2,17 +2,8 @@
 
 namespace App\Http\Requests;
 
-use App\Enums\CharacterCraft;
-use App\Enums\CharacterSkill;
-use App\Enums\FateType;
-use App\Models\PerkVariant;
 use App\Models\RaceTrait as ModelsRaceTrait;
-use App\Rules\CraftPool;
-use App\Rules\Fates;
-use App\Rules\NarrativeCraftsPool;
-use App\Rules\PerkPool;
 use App\Rules\RaceTrait;
-use App\Rules\SkillPool;
 use App\Rules\Subtrait;
 use Illuminate\Foundation\Http\FormRequest;
 
