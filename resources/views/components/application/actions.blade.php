2a6e7bee55bdf1eec866675ac699445fc5ef5d8e
AleXXX_007 <AleXXX_007@inbox.ru>
2022-01-07 18:51:34 +0400

    better confirmation dialogs

--- resources/views/components/application/actions.blade.php
+++ resources/views/components/application/actions.blade.php
@@ -6,7 +6,7 @@
       color="red-300"
       colorHover="red-400"
       colorRing="red-500"
-      confirmationText="return confirm('{{ __('ui.confirm') }}')"
+      confirmationText="return confirm('{{ __('ui.confirm', ['tip' => __('tips.character.force_delete')]) }}')"
       icon="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
       tooltip="{{ isset($tooltip) }}"
     />
@@ -31,7 +31,7 @@
       color="red-300"
       colorHover="red-400"
       colorRing="red-500"
-      confirmationText="return confirm('{{ __('ui.confirm') }}')"
+      confirmationText="return confirm('{{ __('ui.confirm', ['tip' => __('tips.character.delete')]) }}')"
       icon="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
       tooltip="{{ isset($tooltip) }}"
     />
@@ -71,7 +71,7 @@
 				color="gray-200"
 				colorHover="gray-300"
 				colorRing="gray-400"
-				confirmationText="return confirm('{{ __('ui.confirm') }}')"
+				confirmationText="return confirm('{{ __('ui.confirm', ['tip' => __('tips.character.cancel')]) }}')"
 				icon="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
 				tooltip="{{ isset($tooltip) }}"
 			/>
@@ -98,7 +98,7 @@
 				color="red-200"
 				colorHover="red-300"
 				colorRing="red-400"
-				confirmationText="return confirm('{{ __('ui.confirm') }}')"
+				confirmationText="return confirm('{{ __('ui.confirm', ['tip' => __('tips.character.cancel_approval')]) }}')"
 				icon="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
 				tooltip="{{ isset($tooltip) }}"
 			/>
@@ -137,7 +137,7 @@
 				color="green-200"
 				colorHover="green-300"
 				colorRing="green-400"
-				confirmationText="return confirm('{{ __('ui.confirm') }}')"
+				confirmationText="return confirm('{{ __('ui.confirm', ['tip' => __('tips.character.approval')]) }}')"
 				icon="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-6 9l2 2 4-4"
 				tooltip="{{ isset($tooltip) }}"
 			/>    
@@ -152,7 +152,7 @@
 				color="yellow-100"
 				colorHover="yellow-200"
 				colorRing="yellow-300"
-				confirmationText="return confirm('{{ __('ui.confirm') }}')"
+				confirmationText="return confirm('{{ __('ui.confirm', ['tip' => __('tips.character.reapproval')]) }}')"
 				icon="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
 				tooltip="{{ isset($tooltip) }}"
 			/>
