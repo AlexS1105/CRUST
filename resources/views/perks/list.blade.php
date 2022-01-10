47c99fc75a6a33235add45f1610b8190798984c6
AleXXX_007 <AleXXX_007@inbox.ru>
2022-01-10 17:09:46 +0400

    fix

--- resources/views/perks/list.blade.php
+++ resources/views/perks/list.blade.php
@@ -17,11 +17,11 @@
     <script src="{{ asset('js/app.js') }}" defer></script>
   </head>
   <body class="font-sans antialiased bg-white">
-    <div class="p-2 fixed top-0 w-full bg-white border-b">
+    <div class="z-10 p-2 fixed top-0 w-full bg-white border-b">
       @php
         $perkTypeInstance = isset($perkType) ? App\Enums\PerkType::fromValue($perkType) : App\Enums\PerkType::None();
       @endphp
-      <div class="z-10 flex p-1 mb-2 space-x-2">
+      <div class="flex p-1 mb-2 space-x-2">
         <a href="{{ route('perks.list', ['perk_type' => App\Enums\PerkType::Combat]) }}" class="bg-red-200 px-2 rounded-full {{ $perkTypeInstance->isCombat() ? '' : 'opacity-50' }}">
           {{ __('perks.types.combat') }}
         </a>
