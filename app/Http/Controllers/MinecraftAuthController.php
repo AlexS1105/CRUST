9bf48eef4c0576122f06ec49fcfe144fa3c2a808
AleXXX_007 <AleXXX_007@inbox.ru>
2022-01-22 15:38:28 +0400

    fix

--- app/Http/Controllers/MinecraftAuthController.php
+++ app/Http/Controllers/MinecraftAuthController.php
@@ -36,7 +36,7 @@ class MinecraftAuthController extends Controller
                         $user = $account->user;
                     }
 
-                    if (config('services.minecraft.characters_auth')) {
+                    if (config('services.minecraft.accounts_auth')) {
                         throw new Exception(__('minecraft.auth_accounts'));
                     }
                 }
