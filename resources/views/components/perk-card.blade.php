37765a95b34ee49078aa87a73caac92bc2594ff1
AleXXX_007 <AleXXX_007@inbox.ru>
2022-07-14 11:57:10 +0400

    toggle perk permission fix

--- resources/views/components/perk-card.blade.php
+++ resources/views/components/perk-card.blade.php
@@ -4,29 +4,31 @@
       <div>
         {{ $perk->name }}
       </div>
-      @if($character->vox > 1 && !$perkVariant->pivot->active || $perkVariant->pivot->active)
-        <form method="POST" action="{{ route('characters.perks.toggle', ['character' => $character, 'perkVariant' => $perkVariant]) }}">
-          @csrf
-          @method('PATCH')
+      @can('togglePerks', $character)
+        @if($character->vox > 1 && !$perkVariant->pivot->active || $perkVariant->pivot->active)
+          <form method="POST" action="{{ route('characters.perks.toggle', ['character' => $character, 'perkVariant' => $perkVariant]) }}">
+            @csrf
+            @method('PATCH')
 
-          <a
-            class="cursor-pointer text-blue-600"
-            onclick="if (confirm('{{ __('ui.confirm', ['tip' => __('tips.perk.'.($perkVariant->pivot->active ? 'deactivation' : 'activation'))]) }}')) {
-              event.preventDefault();
-              this.closest('form').submit();
-            }"
-            title="{{ __('perks.'.($perkVariant->pivot->active ? 'deactivate' : 'activate')) }}">
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              @if ($perkVariant->pivot->active)
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 9v6m4-6v6m7-3a9 9 0 11-18 0 9 9 0 0118 0z" />
-              @else
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z" />
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-              @endif
-            </svg>
-          </a>
-        </form>
-      @endif
+            <a
+              class="cursor-pointer text-blue-600"
+              onclick="if (confirm('{{ __('ui.confirm', ['tip' => __('tips.perk.'.($perkVariant->pivot->active ? 'deactivation' : 'activation'))]) }}')) {
+                event.preventDefault();
+                this.closest('form').submit();
+              }"
+              title="{{ __('perks.'.($perkVariant->pivot->active ? 'deactivate' : 'activate')) }}">
+              <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                @if ($perkVariant->pivot->active)
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 9v6m4-6v6m7-3a9 9 0 11-18 0 9 9 0 0118 0z" />
+                @else
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z" />
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                @endif
+              </svg>
+            </a>
+          </form>
+        @endif
+      @endcan
     </div>
     <div class="flex p-2 text-center font-bold text-lg border-gray-400 border-l">
       {{ $perk->cost }}
