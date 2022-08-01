84ffe7a81e40584e7353b4df113a041d6c08eff9
AleXXX_007 <AleXXX_007@inbox.ru>
2022-08-01 11:01:09 +0400

    narrative crafts accordeon

--- resources/views/characters/show.blade.php
+++ resources/views/characters/show.blade.php
@@ -172,14 +172,21 @@
             @endif
 
             @if(count($character->narrativeCrafts))
+              <div id="accordion-collapse" data-accordion="collapse">
               @foreach($character->narrativeCrafts as $craft)
-                <div class="my-2">
-                  <div class="text-lg font-semibold">
-                    {{ $craft->name }}
+                <h2 id="accordion-collapse-heading-{{$loop->iteration}}">
+                  <button type="button" class="flex items-center justify-between w-full p-2 font-medium text-left text-gray-500 border border-gray-200 {{$loop->last ? "" : "border-b-0"}}" data-accordion-target="#accordion-collapse-body-{{$loop->iteration}}" aria-expanded="true" aria-controls="accordion-collapse-body-{{$loop->iteration}}">
+                    <span>{{ $craft->name }}</span>
+                    <svg data-accordion-icon class="w-6 h-6 rotate-180 shrink-0" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                  </button>
+                </h2>
+                <div id="accordion-collapse-body-{{$loop->iteration}}" class="hidden" aria-labelledby="accordion-collapse-heading-{{$loop->iteration}}">
+                  <div class="p-2 font-light border {{$loop->last ? "border-t-0" : "border-b-0"}} border-gray-200">
+                    <p class="mb-2 text-gray-500">{{ $craft->description }}</p>
                   </div>
-                  {{ $craft->description }}
                 </div>
               @endforeach
+              </div>
             @endif
           </div>
         @endif
