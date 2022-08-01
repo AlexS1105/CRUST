80d38fd6c1fa95a1039b3faac2a51e11aa131d5a
AleXXX_007 <AleXXX_007@inbox.ru>
2022-08-01 11:00:48 +0400

    flowbite installed

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,6 +17,7 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="https://unpkg.com/flowbite@1.4.7/dist/flowbite.js"></script>
   </head>
   <body class="font-sans antialiased bg-gray-100">
     @include('layouts.navigation')
