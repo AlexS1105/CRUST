41219b283c674e6438e9cac0c56bfec2e142b8dc
AleXXX_007 <AleXXX_007@inbox.ru>
2022-08-02 21:48:10 +0400

    hotfix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,7 +16,7 @@
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ asset('js/app.js?v=1') }}" defer></script>
     <script src="https://unpkg.com/flowbite@1.4.7/dist/flowbite.js"></script>
   </head>
   <body class="font-sans antialiased bg-gray-100">
