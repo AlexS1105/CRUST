115216f31ddf0e63593e3d75592a85dafdc5e19f
AleXXX_007 <AleXXX_007@inbox.ru>
2022-09-22 13:55:16 +0400

    perk view fix

--- resources/views/perks/edit.blade.php
+++ resources/views/perks/edit.blade.php
@@ -13,7 +13,7 @@
       <x-form.card>
         <x-form.input name="name" required maxlength="256" placeholder="{{ __('perks.placeholder.name') }}" :value="old('name', $perk->name)"/>
           <x-form.textarea name="general_description" maxlength="5096" onfocus="preview(this)" placeholder="{{ __('perks.placeholder.description') }}" wrap="off">
-            {{ old('general_description') }}
+            {{ old('general_description', $perk->general_description) }}
           </x-form.textarea>
         <x-form.checkbox name="combat" value="{{ old('combat', $perk->type->hasFlag(App\Enums\PerkType::Combat)) }}" />
         <x-form.checkbox name="attack" value="{{ old('attack', $perk->type->hasFlag(App\Enums\PerkType::Attack)) }}" />
