138f09bdad11cb047a18756efacb76cfb234066b
AlexS1105 <AleXXX_007@inbox.ru>
2022-10-14 10:14:27 +0400

    unused api routes removed

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,5 @@
 <?php
 
-use App\Http\Controllers\AuthController;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,9 +10,3 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
-Route::middleware('auth:api')->get('/login', AuthController::class);
