86a9115bb8d1fc35fbdd742b7ef0dd7f156dbfa9
AlexS1105 <AleXXX_007@inbox.ru>
2022-10-14 10:14:27 +0400

    telescope installed

--- config/app.php
+++ config/app.php
@@ -172,6 +172,7 @@ return [
         App\Providers\AuthServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        App\Providers\TelescopeServiceProvider::class,
         App\Providers\DiscordServiceProvider::class,
 
         App\Extensions\Passwords\PasswordResetServiceProvider::class,
