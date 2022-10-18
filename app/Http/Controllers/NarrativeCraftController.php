02270710325f8342fbcf925bc762e0ce16b033bf
AlexS1105 <AleXXX_007@inbox.ru>
2022-10-18 15:18:34 +0400

    narrative crafts refactor

--- app/Http/Controllers/NarrativeCraftController.php
+++ app/Http/Controllers/NarrativeCraftController.php
@@ -10,43 +10,42 @@ class NarrativeCraftController extends Controller
 {
     public function create(Character $character)
     {
-        $this->authorize('updateCharsheetGm', $character);
+        $this->authorize('update-charsheet-gm', $character);
+
         return view('crafts.create', compact('character'));
     }
 
     public function store(NarrativeCraftRequest $request, Character $character)
     {
-        $this->authorize('updateCharsheetGm', $character);
-        $validated = $request->validated();
-        $character->narrativeCrafts()->create($validated);
+        $this->authorize('update-charsheet-gm', $character);
 
-        return redirect()->route('characters.show', $character);
+        $character->narrativeCrafts()->create($request->validated());
+
+        return to_route('characters.show', $character);
     }
 
     public function edit(Character $character, NarrativeCraft $narrativeCraft)
     {
         $this->authorize('update', $character);
-        return view('crafts.edit', [
-            'character' => $character,
-            'narrativeCraft' => $narrativeCraft,
-        ]);
+
+        return view('crafts.edit', compact('character', 'narrativeCraft'));
     }
 
     public function update(NarrativeCraftRequest $request, Character $character, NarrativeCraft $narrativeCraft)
     {
         $this->authorize('update', $character);
-        $validated = $request->validated();
-        $narrativeCraft->update($validated);
 
-        return redirect()->route('characters.show', $character);
+        $narrativeCraft->update($request->validated());
+
+        return to_route('characters.show', $character);
     }
 
     public function destroy(Character $character, NarrativeCraft $narrativeCraft)
     {
         $this->authorize('update', $character);
-        $this->authorize('manageIdeas', $character);
+
         $narrativeCraft->delete();
 
-        return redirect()->route('characters.show', $character);
+        return to_route('characters.show', $character);
     }
 }
