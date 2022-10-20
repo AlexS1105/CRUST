8407e1166d0ba11de199cafab98e951da5250635
AlexS1105 <AleXXX_007@inbox.ru>
2022-10-20 10:29:07 +0400

    minor refactor

--- app/Jobs/Discord/TicketJob.php
+++ app/Jobs/Discord/TicketJob.php
@@ -9,9 +9,4 @@ class TicketJob extends CharacterJob
 {
     public $tries = 3;
     public $backoff = 5;
-
-    public function __construct(Character $character)
-    {
-        parent::__construct($character);
-    }
 }
--- app/Services/MinecraftService.php
+++ app/Services/MinecraftService.php
@@ -15,45 +15,49 @@ class MinecraftService
         try {
             $login = $request->login;
             $password = $request->password;
+            $this->checkUser($login, $password);
 
-            if (! isset($login) || ! isset($password)) {
-                throw new Exception(__('minecraft.auth.credentials'));
-            }
-
-            $character = Character::firstWhere(DB::raw('BINARY `login`'), $login);
-
-            if (isset($character) && $character->registered) {
-                $user = $character->user;
-
-                if (! config('services.minecraft.characters_auth')) {
-                    throw new Exception(__('minecraft.auth.characters'));
-                }
-            } else {
-                $account = Account::firstWhere(DB::raw('BINARY `login`'), $login);
-
-                if (isset($account)) {
-                    $user = $account->user;
-
-                    if (! config('services.minecraft.accounts_auth')) {
-                        throw new Exception(__('minecraft.auth.accounts'));
-                    }
-                }
-            }
-
-            if (isset($user) && Hash::check($password, $user->password)) {
-                if ($user->isBanned) {
-                    throw new Exception($user->ban->message);
-                }
-
-                return $this->launcherResponse("OK:{$login}");
-            }
-
-            throw new Exception();
+            return $this->launcherResponse("OK:{$login}");
         } catch (Exception $e) {
             return $this->launcherResponse(
                 empty($e->getMessage()) ?
                     __('minecraft.auth.failed') : $e->getMessage()
             );
+        }
+    }
+
+    private function checkUser($login, $password)
+    {
+        if (! isset($login) || ! isset($password)) {
+            throw new Exception(__('minecraft.auth.credentials'));
+        }
+
+        $character = Character::firstWhere(DB::raw('BINARY `login`'), $login);
+
+        if (isset($character) && $character->registered) {
+            $user = $character->user;
+
+            if (! config('services.minecraft.characters_auth')) {
+                throw new Exception(__('minecraft.auth.characters'));
+            }
+        } else {
+            $account = Account::firstWhere(DB::raw('BINARY `login`'), $login);
+
+            if (isset($account)) {
+                $user = $account->user;
+
+                if (! config('services.minecraft.accounts_auth')) {
+                    throw new Exception(__('minecraft.auth.accounts'));
+                }
+            }
+        }
+
+        if (isset($user)) {
+            if (Hash::check($password, $user->password) && $user->isBanned) {
+                throw new Exception($user->ban->message);
+            }
+        } else {
+            throw new Exception();
         }
     }
 
