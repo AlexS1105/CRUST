9bcde57f94f2d51b48fbe50e257442c5b84aed60
AlexS1105 <AleXXX_007@inbox.ru>
2022-10-27 11:12:09 +0400

    base layouts

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,34 +1,17 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+@extends('layouts.base')
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+@section('body')
+    <header class="bg-white shadow">
+        @include('layouts.navigation')
 
-    <link rel="icon" href="{{ url('favicon.ico') }}">
+        <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                @yield('header')
+            </h2>
+        </div>
+    </header>
 
-    <!-- Fonts -->
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
-
-    <!-- Scripts -->
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-    <script src="https://unpkg.com/flowbite@1.4.7/dist/flowbite.js"></script>
-</head>
-<body class="font-sans antialiased bg-gray-100">
-@include('layouts.navigation')
-
-<!-- Page Heading -->
-<header class="bg-white shadow">
-    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-        {{ $header }}
-    </div>
-</header>
-
-<!-- Page Content -->
-<main>
-    {{ $slot }}
-</main>
-</body>
-</html>
+    <main>
+        @yield('content')
+    </main>
+@endsection
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,21 +1,15 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+@extends('layouts.base')
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+@section('body')
+    <div class="font-sans text-gray-900 antialiased">
+        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
+            <a href="/">
+                <x-application-logo class="w-80 h-80 fill-current text-gray-500"/>
+            </a>
 
-    <!-- Fonts -->
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
-
-    <!-- Scripts -->
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-</head>
-<body>
-<div class="font-sans text-gray-900 antialiased">
-    {{ $slot }}
-</div>
-</body>
-</html>
+            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+                @yield('content')
+            </div>
+        </div>
+    </div>
+@endsection
