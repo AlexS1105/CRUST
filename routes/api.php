fb7a54b7a96c01738bcdf5b05d8ac7d6629376e3
AleXXX_007 <AleXXX_007@inbox.ru>
2022-10-31 19:36:49 +0400

    return mediawiki auth endpoints

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,11 @@ use Illuminate\Support\Facades\Route;
 Route::patch('/unverify-user', [DiscordController::class, 'unverifyUser'])
     ->middleware('auth.apikey')
     ->name('discord.unverify');
+
+// MediaWiki OAuth2.0 endpoints
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::middleware('auth:api')->get('/login', AuthController::class);
