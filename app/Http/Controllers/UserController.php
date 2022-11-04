d3c6414b82135e4f87c660ddff05f9f70b48e316
AleXXX_007 <AleXXX_007@inbox.ru>
2022-11-04 17:20:48 +0400

    user search fix

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -19,7 +19,7 @@ class UserController extends Controller
     public function index(Request $request)
     {
         $search = $request->search;
-        $users = User::search($search)
+        $users = User::search($search, 'login')
             ->latest('created_at')
             ->sortable()
             ->paginate(20);
