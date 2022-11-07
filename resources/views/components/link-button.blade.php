b8ab741caa6e674f10da63849c71476f82d2f353
AlexS1105 <AleXXX_007@inbox.ru>
2022-11-07 16:48:28 +0400

    logs refactor

---
+++ resources/views/components/link-button.blade.php
@@ -0,0 +1,3 @@
+<a {{ $attributes->merge(['class' => 'bg-blue-600 text-white p-2 font-bold text-sm rounded-full hover:bg-blue-800 focus:ring-4']) }}>
+    {{ $slot }}
+</a>
--- resources/views/logs/index.blade.php
+++ resources/views/logs/index.blade.php
@@ -3,15 +3,9 @@
 @section('header', __('logs.index'))
 
 @section('content')
-    <x-container class="max-w-6xl space-y-8">
-        <div class="bg-white rounded-xl max-w-3xl mx-auto shadow-lg place-self-start p-6 space-y-4">
-            <div>
-                <a href="{{ route('logs.crust') }}">{{ __('logs.crust') }}</a>
-            </div>
-
-            <div>
-                <a href="{{ route('logs.ingame') }}">{{ __('logs.ingame') }}</a>
-            </div>
-        </div>
+    <x-container>
+        <x-card class="flex flex-wrap gap-2">
+            <x-link-button href="{{ route('logs.crust') }}">{{ __('logs.crust') }}</x-link-button>
+        </x-card>
     </x-container>
 @endsection
