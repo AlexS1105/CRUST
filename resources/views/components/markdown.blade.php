75ed2a6aaa3e902c1d3cf77ab9adb0209597b665
AleXXX_007 <AleXXX_007@inbox.ru>
2023-06-04 13:13:36 +0400

    dark theme marker color ifx

--- resources/views/components/markdown.blade.php
+++ resources/views/components/markdown.blade.php
@@ -1,1 +1,1 @@
-<div {{ $attributes->merge(['class' => 'prose dark:prose-invert markdown dark:text-gray-200']) }}>{{ $slot }}</div>
+<div {{ $attributes->merge(['class' => 'prose dark:prose-invert markdown dark:text-gray-200 dark:marker:text-gray-400']) }}>{{ $slot }}</div>
