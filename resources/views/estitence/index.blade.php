b97272eea1e2d30607f4951fe319e00dc63a214f
AleXXX_007 <AleXXX_007@inbox.ru>
2023-07-01 13:44:53 +0400

    estitence logs view fix

--- resources/views/estitence/index.blade.php
+++ resources/views/estitence/index.blade.php
@@ -11,45 +11,51 @@
                 </x-link>
             @endcan
             @if(count($character->estitenceLogs))
-                <table class="table-auto w-full border mt-2">
-                    <thead class="border bg-gray-200">
-                    <tr>
-                        <th class="px-4 py-2 border border-gray-400">
+                <x-table>
+                    <x-slot name="heading">
+                        <x-table.header>
                             {{ __('label.date') }}
-                        </th>
-                        <th class="px-4 py-2 border border-gray-400">
+                        </x-table.header>
+                        <x-table.header>
                             {{ __('label.amount') }}
-                        </th>
-                        <th class="px-4 py-2 border border-gray-400">
+                        </x-table.header>
+                        <x-table.header>
                             {{ __('label.reason') }}
-                        </th>
-                        <th class="px-4 py-2 border border-gray-400">
+                        </x-table.header>
+                        <x-table.header>
                             {{ __('label.issued_by') }}
-                        </th>
-                    </tr>
-                    </thead>
-                    <tbody>
+                        </x-table.header>
+                    </x-slot>
+
                     @foreach ($character->estitenceLogs->sortBy('created_at') as $estitenceLog)
-                        <tr class="py-2 hover:{{ $estitenceLog->delta > 0 ? 'bg-green' : 'bg-red' }}-200 text-center {{ $estitenceLog->delta > 0 ? 'bg-green' : 'bg-red' }}-100">
-                            <td class="px-4 py-2 border border-gray-400">
-                                {{ $estitenceLog->created_at }}
-                            </td>
-                            <td class="px-4 py-2 border border-gray-400 leading-none">
-                                {{ $estitenceLog->delta }}
-                                <div class="text-xs">
-                                    {{ $estitenceLog->before }} -> {{ $estitenceLog->after }}
+                        <x-table.row class="dark:text-gray-300 py-2 text-center">
+                            <x-table.cell class="px-4 py-2 border border-gray-400 {{ $estitenceLog->delta > 0 ? 'bg-green' : 'bg-red' }}-100 dark:{{ $estitenceLog->delta > 0 ? 'bg-green' : 'bg-red' }}-400">
+                                <div class="dark:drop-shadow-xs">
+                                    {{ $estitenceLog->created_at }}
                                 </div>
-                            </td>
-                            <td class="px-4 py-2 border border-gray-400 min-w-full">
-                                {{ $estitenceLog->reason }}
-                            </td>
-                            <td class="px-4 py-2 border border-gray-400">
-                                {{ $estitenceLog->issuedBy?->login }}
-                            </td>
-                        </tr>
+                            </x-table.cell>
+                            <x-table.cell class="px-4 py-2 border border-gray-400 {{ $estitenceLog->delta > 0 ? 'bg-green' : 'bg-red' }}-100 dark:{{ $estitenceLog->delta > 0 ? 'bg-green' : 'bg-red' }}-400">
+                                <div class="dark:drop-shadow-xs">
+                                    {{ $estitenceLog->delta }}
+
+                                    <div class="text-xs">
+                                        {{ $estitenceLog->before }} -> {{ $estitenceLog->after }}
+                                    </div>
+                                </div>
+                            </x-table.cell>
+                            <x-table.cell class="px-4 py-2 border border-gray-400 {{ $estitenceLog->delta > 0 ? 'bg-green' : 'bg-red' }}-100 dark:{{ $estitenceLog->delta > 0 ? 'bg-green' : 'bg-red' }}-400">
+                                <div class="dark:drop-shadow-xs">
+                                    {{ $estitenceLog->reason }}
+                                </div>
+                            </x-table.cell>
+                            <x-table.cell class="px-4 py-2 border border-gray-400 {{ $estitenceLog->delta > 0 ? 'bg-green' : 'bg-red' }}-100 dark:{{ $estitenceLog->delta > 0 ? 'bg-green' : 'bg-red' }}-400">
+                                <div class="dark:drop-shadow-xs">
+                                    {{ $estitenceLog->issuedBy?->login }}
+                                </div>
+                            </x-table.cell>
+                        </x-table.row>
                     @endforeach
-                    </tbody>
-                </table>
+                </x-table>
             @else
                 <p class="pt-4 text-xl font-semibold text-gray-500 dark:text-gray-300 text-center">
                     {{ __('estitence.empty') }}
